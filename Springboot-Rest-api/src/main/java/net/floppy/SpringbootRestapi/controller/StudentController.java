91c0b900c1f04420c80c62934cd5c46836fdf3a6
Dinesh <30436687+Dinesh333s@users.noreply.github.com>
2024-01-11 00:33:36 +0530

    Minor Changes on controller

--- Springboot-Rest-api/src/main/java/net/floppy/SpringbootRestapi/controller/StudentController.java
+++ Springboot-Rest-api/src/main/java/net/floppy/SpringbootRestapi/controller/StudentController.java
@@ -2,64 +2,85 @@ package net.floppy.SpringbootRestapi.controller;
 
 import net.floppy.SpringbootRestapi.bean.Student;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("student")
 public class StudentController {
 
     //http://localhost:8080/student
-    @GetMapping("student")
-    public Student get_student() {
+    @GetMapping
+    public ResponseEntity<Student> get_student() {
         Student student = new Student(
                 1,
                 "Dinesh",
                 "s"
         );
-        return student;
+//        return new ResponseEntity<>(student, HttpStatus.OK);
+//        return ResponseEntity.ok(student);
+        return ResponseEntity.ok().header("Custom-Header", "Dinesh").body(student);
     }
 
     //http://localhost:8080/students
     @GetMapping("students")
-    public List<Student> get_students() {
+    public ResponseEntity<List<Student>> get_students() {
         List<Student> students = new ArrayList<>();
         students.add(new Student(1, "Raj", "kumar"));
         students.add(new Student(2, "Roman", "powel"));
         students.add(new Student(3, "Joe", "Weller"));
         students.add(new Student(4, "Mr", "Whoisthebose"));
-        return students;
+        return ResponseEntity.ok(students);
     }
 
     //http://localhost:8080/student/1/dinesh/s
     //{id} - URI template
-    @GetMapping("student/{id}/{first-name}/{last-name}")
-    public Student pathvariable(@PathVariable("id") int studentid,
+    @GetMapping("{id}/{first-name}/{last-name}")
+    public ResponseEntity<Student> pathvariable(@PathVariable("id") int studentid,
                                 @PathVariable("first-name") String fistName,
                                 @PathVariable("last-name") String lastName) {
-        return new Student(studentid, fistName, lastName);
+        Student student = new Student(studentid, fistName, lastName);
+        return ResponseEntity.ok(student);
     }
 
     //http://localhost:8080/students/query?id=1&firstName=Dinesh&lastName=s
     //RequestParam extract the value of queryparameter in a request url
-   @GetMapping("student/query")
-    public Student requestparam(@RequestParam int id,
+   @GetMapping("query")
+    public ResponseEntity<Student> requestparam(@RequestParam int id,
                                 @RequestParam  String firstName,
                                 @RequestParam String lastName)
     {
-        return new Student(id,firstName,lastName);
+        Student student=  new Student(id,firstName,lastName);
+        return ResponseEntity.ok(student);
     }
 
     //http://localhost:8080/student/create
     //RequestBody - retrieve HTTP request body JSON and convert to java object
-    @PostMapping("student/create")
+    @PostMapping("create")
     @ResponseStatus(HttpStatus.CREATED)
-    public Student create(@RequestBody Student student)
+    public ResponseEntity<Student> create(@RequestBody Student student)
     {
         System.out.println(student.getId());
         System.out.println(student.getFirst_name());
         System.out.println(student.getLast_name());
-        return student;
+        return new ResponseEntity<>(student,HttpStatus.CREATED);
+    }
+    @PutMapping("{id}/update")
+    public ResponseEntity<Student> update(@RequestBody Student student,@PathVariable("id") int studentid)
+    {
+        System.out.println(student.getId());
+        System.out.println(student.getFirst_name());
+        System.out.println(student.getLast_name());
+        return ResponseEntity.ok(student);
+    }
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<String> delete(@PathVariable("id") int studentid)
+    {
+        System.out.println(studentid);
+        return ResponseEntity.ok("Resource Deleted Successfully");
     }
 }
