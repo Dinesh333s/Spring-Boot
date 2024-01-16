2f9021932525120c3fa871da1ae70b3e7b82c8e8
Dinesh <30436687+Dinesh333s@users.noreply.github.com>
2024-01-17 00:18:32 +0530

    Specific & Global Exception Added

--- Springboot-Restful-webservices-demo/src/main/java/com/floppy/SpringbootRestfulwebservicesdemo/exception/GlobalExceptionHandler.java
+++ Springboot-Restful-webservices-demo/src/main/java/com/floppy/SpringbootRestfulwebservicesdemo/exception/GlobalExceptionHandler.java
@@ -36,4 +36,17 @@ public class GlobalExceptionHandler {
         );
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handleEmailAlreadyExistsException(Exception exception,
+                                                                          WebRequest webRequest)
+    {
+        ErrorDetails errorDetails = new ErrorDetails(
+                LocalDateTime.now(),
+                exception.getMessage(),
+                webRequest.getDescription(false),
+                "INTERNAL SERVER ERROR"
+        );
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
