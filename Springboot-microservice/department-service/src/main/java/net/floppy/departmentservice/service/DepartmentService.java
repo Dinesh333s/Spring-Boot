47779245bdc8aee092d87ca63263f12dc210945f
Dinesh <30436687+Dinesh333s@users.noreply.github.com>
2024-01-29 23:38:58 +0530

    Delete Springboot-microservice directory

--- Springboot-microservice/department-service/src/main/java/net/floppy/departmentservice/service/DepartmentService.java
+++
@@ -1,7 +0,0 @@
-package net.floppy.departmentservice.service;
-
-import net.floppy.departmentservice.dto.DepartmentDto;
-
-public interface DepartmentService {
-    DepartmentDto saveDepartment(DepartmentDto departmentDto);
-}
